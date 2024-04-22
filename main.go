2dcff6c799be6295205ff51344d3e85b4cd7c93d
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-04-22 18:43:33 -0300

    init calculator

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func add(number1, number2 int) int {
+	return number1 + number2
+}
+
+func sub(number1, number2 int) int {
+	return number1 - number2
+}
+
+func mul(number1, number2 int) int {
+	return number1 * number2
+}
+
+func div(number1, number2 int) (int, error) {
+	if number2 == 0 {
+		return 0, errors.New("Error: Division by zero")
+	} else {
+		return number1 / number2, nil
+	}
+}
+
+func main() {
+	fmt.Println(add(10, 20))
+	fmt.Println(sub(10, 20))
+	fmt.Println(mul(10, 20))
+	result, err := div(10, 10)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+}
